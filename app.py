dee262ab1b274ac2fe17d3a3c58eaf903a97450b
Adithya P Patel <70311653+adithya5777@users.noreply.github.com>
2023-09-04 12:57:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from sklearn import preprocessing
-import pickle
+#import pickle
 import numpy as np
 import pandas as pd
 import joblib as jb
